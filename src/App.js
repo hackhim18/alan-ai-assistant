a1587b761048aa4a95287bcb51384d4637662bfb
hackhim18 <triphimanshu18@gmail.com>
2021-06-21 15:43:26 +0530

    c3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { useEffect} from 'react'
+import React, { useState,useEffect} from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
+
+import NewsCards from './components/NewsCards/NewsCards'
  
 const alanKey = 'e83c6d4a0a2ea512201cde347f5d42302e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => { 
+    const [newsArticles, setNewsArticles] = useState([]);
 
     useEffect(() =>{
         alanBtn({
             key: alanKey,
             onCommand: ({ command,articles }) =>{
                 if(command === 'newHeadlines'){
-               console.log(articles);
+               setNewsArticles(articles);
              }
             
             }
         })
-
     },[])
 
      return (
          <div>
              <h1>Alan AI News</h1>
+             <NewsCards articles={newsArticles}/>
          </div>
      )
  }
