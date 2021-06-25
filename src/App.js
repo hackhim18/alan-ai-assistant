f78c9df7ba3a8190f1106544ab29dae55d625014
Himanshu Tripathi <65823429+hackhim18@users.noreply.github.com>
2021-06-25 21:50:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ const App = () => {
       
       </div>
 
-      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <NewsCards articles={ newsArticles} activeArticle={activeArticle} />
       
       {!newsArticles.length ? (
         <div className={classes.footer}>
